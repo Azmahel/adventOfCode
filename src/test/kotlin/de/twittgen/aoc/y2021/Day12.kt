07fc430ffdb24696c4b76332f810dbf5e14e3c2d
tristan.wittgen <tristanwittgen@gmail.com>
2021-12-12 10:58:06 +0100

    #day12 2021 refactorings

--- src/test/kotlin/de/twittgen/aoc/y2021/Day12.kt
+++ src/test/kotlin/de/twittgen/aoc/y2021/Day12.kt
@@ -10,9 +10,7 @@ class Day12 {
     val example2 = "dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc".parse()
     val example3 = "fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX\npj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW".parse()
 
-    private fun String.parse() = lines().map { line -> line.split("-") }.map { (first, second) ->
-        first.toCave() to second.toCave()
-    }
+    private fun String.parse() = lines().map { it.split("-") }.map { (a, b) -> a.toCave() to b.toCave() }
 
     private fun Cave.getAdjacent(map: List<Pair<Cave,Cave>>) =
         map.mapNotNull { when {
@@ -21,18 +19,16 @@ class Day12 {
             else -> null
         } }
 
-    private  fun List<Pair<Cave,Cave>>.findPaths(
-        allowSmallDoubles: Boolean = false,
-        previous: List<Cave> = listOf(Start),
+    private  fun List<Pair<Cave,Cave>>.findPaths(allowSmallDouble: Boolean = false, previous: List<Cave> = listOf(Start),
     ): List<List<Cave>> {
         if (previous.last() == End) return listOf(previous)
-        val adjacent = previous.last().getAdjacent(this).filterNot { it in getBlocked(previous, allowSmallDoubles) }
-        return adjacent.flatMap { findPaths(allowSmallDoubles, previous + it) }.filterNot { it.isEmpty() }
+        val adjacent = previous.last().getAdjacent(this).filterNot { it in previous.blocked(allowSmallDouble) }
+        return adjacent.flatMap { findPaths(allowSmallDouble, previous + it) }.filterNot { it.isEmpty() }
     }
 
-    private fun getBlocked(path: List<Cave>, allowSmallDoubles: Boolean): List<Cave> {
-        val spentSmall = path.filterIsInstance<Small>()
-        return if(!allowSmallDoubles || (spentSmall.hasDouble())) (spentSmall + Start) else listOf(Start)
+    private fun List<Cave>.blocked(allowSmallDouble: Boolean): List<Cave> {
+        val spentSmall = filterIsInstance<Small>()
+        return if(!allowSmallDouble || (spentSmall.hasDouble())) (spentSmall + Start) else listOf(Start)
     }
 
     private fun  List<Cave>.hasDouble(): Boolean = toSet().size != size
@@ -52,22 +48,14 @@ class Day12 {
 
     @Test
     fun example() {
-        val result1 = example.findPaths()
-        assertEquals(10, result1.size)
-        val result2 = example2.findPaths()
-        assertEquals(19, result2.size)
-        val result3 = example3.findPaths()
-        assertEquals(226, result3.size)
+        val result = listOf(example.findPaths(), example2.findPaths(), example3.findPaths())
+        assertEquals(listOf(10, 19, 226), result.map{ it.size })
     }
 
     @Test
     fun example2() {
-        val result1 = example.findPaths(true)
-        assertEquals(36, result1.size)
-        val result2 = example2.findPaths(true)
-        assertEquals(103, result2.size)
-        val result3 = example3.findPaths(true)
-        assertEquals(3509, result3.size)
+        val result = listOf(example.findPaths(true), example2.findPaths(true), example3.findPaths(true))
+        assertEquals(listOf(36, 103, 3509), result.map { it.size })
     }
 
     @Test
