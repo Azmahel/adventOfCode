9bd38baac730f6f2431af8e562fe3869e882868f
tristan.wittgen <tristanwittgen@gmail.com>
2022-12-03 17:32:25 +0100

    #2022 day 3

---
+++ src/test/kotlin/de/twittgen/aoc/y2022/Day3.kt
@@ -0,0 +1,75 @@
+package de.twittgen.aoc.y2022
+
+import de.twittgen.aoc.y2019.shared.util.FileUtil
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day3 {
+    val input by lazy { parseInput(FileUtil.readInput("2022/day3")) }
+    val example = """
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+    """.trimIndent()
+
+    val alphabet = ('a'..'z')+('A'..'Z')
+    val scores: Map<Char, Int> = (alphabet).mapIndexed { i, c ->
+        c to i+1
+    }.toMap()
+    private fun parseInput(input: String) =
+        input.lines().map {
+           it.chunked(it.length/2).let {
+               it[0].toSet() to it[1].toSet()
+           }
+        }
+
+    @Test
+    fun part1Example() {
+        val result = parseInput(example)
+            .map { (a, b) -> a.intersect(b).single() }
+            .sumOf { scores[it]!! }
+        println(result)
+        assertEquals(157, result)
+    }
+
+    @Test
+    fun part1() {
+        val result = input
+            .map { (a, b) -> a.intersect(b).single() }
+            .sumOf { scores[it]!! }
+        println(result)
+        assertEquals(8085, result)
+    }
+
+    @Test
+    fun part2Example() {
+        val result = parseInput(example)
+            .findBadges()
+            .sumOf { scores[it]!! }
+
+        println(result)
+        assertEquals(70, result)
+    }
+
+    @Test
+    fun part2() {
+        val result = input
+            .findBadges()
+            .sumOf { scores[it]!! }
+
+        println(result)
+        assertEquals(2515, result)
+    }
+
+    private fun List<Pair<Set<Char>, Set<Char>>>.findBadges() = this
+        .chunked(3)
+        .map { group ->
+            group
+                .map { it.first + it.second }
+                .fold(alphabet.toSet()) { a, b -> a.intersect(b) }
+                .single()
+        }
+}
