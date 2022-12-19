92b69a6943ac6b116961504f576b6470790d8121
tristan.wittgen <tristanwittgen@gmail.com>
2022-12-19 23:29:48 +0100

    #2020 day 15 even faster

--- src/test/kotlin/de/twittgen/aoc/y2020/Day15.kt
+++ src/test/kotlin/de/twittgen/aoc/y2020/Day15.kt
@@ -1,26 +1,22 @@
 package de.twittgen.aoc.y2020
 
 import de.twittgen.aoc.Day
-import de.twittgen.aoc.Day.TestType.SLOW
 
-class Day15 : Day<List<Long>>() {
+class Day15 : Day<List<Int>>() {
     override val example = "3,1,2"
 
-    override fun String.parse() = split(',').map { it.toLong() }
+    override fun String.parse() = split(',').map { it.toInt() }
 
     init {
         part1(1836, 475) { playGame(it, 2020) }
         part2(362, 11261) { playGame(it, 30000000) }
     }
 
-    private val loop = Regex(".*(.+)\\1\\1")
-    private fun playGame(start :List<Long>, until: Long ): Long {
-        val occurrences = LinkedHashMap<Long, Long>(minOf(3_500_000, until).toInt()).apply {
-            putAll(start.mapIndexed { i, it -> it to i.toLong()+1 })
-        }
-        var last = start.last().toLong()
-        for (i in (start.size.toLong()) until until) {
-            last = (occurrences[last]?.let { i- it } ?: 0).also {  occurrences[last] = i }
+    private fun playGame(start :List<Int>, until: Int): Int {
+        val oc = IntArray(until).also { start.forEachIndexed { i, n-> it[n] = i+1 } }
+        var last = start.last()
+        for (i in start.size until until) {
+            last = oc[last].let { if(it == 0) 0 else (i - it) }.also { oc[last] = i }
         }
         return last
     }
