03c31d3c9544f6faa6a562efe982de6637307a77
tristan.wittgen <tristanwittgen@gmail.com>
2022-12-20 14:23:48 +0100

    #2022 day20

---
+++ src/test/kotlin/de/twittgen/aoc/y2022/Day20.kt
@@ -0,0 +1,41 @@
+package de.twittgen.aoc.y2022
+
+import de.twittgen.aoc.Day
+
+class Day20 : Day<List<Long>>() {
+    override fun String.parse() = lines().map(String::toLong)
+
+    private val key = 811589153
+    init {
+        part1(3, 8302) { it.mix().score() }
+        part2(1623178306, 656575624777) { it.map {i -> i *key }.mix(10).score() }
+    }
+
+    private fun List<Long>.mix(times: Int = 1): List<Long> {
+        val indexed = mapIndexed { index, i -> index to i  }
+        val mixed = indexed.toMutableList()
+        repeat(times) {
+            indexed.forEach { pivot ->
+                val i = mixed.indexOf(pivot)
+                mixed.removeAt(i)
+                val insertAt = (i + pivot.second).mod(mixed.size)
+                mixed.add(insertAt, pivot)
+            }
+        }
+        return mixed.map { it.second }
+    }
+
+    private fun List<Long>.score() =
+        indexOfFirst { it == 0L }.let { listOf(1000, 2000, 3000).sumOf { c -> get((it + c) % size) } }
+
+
+    override val example = """
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
+    """.trimIndent()
+}
