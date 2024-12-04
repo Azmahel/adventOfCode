56bd61b7784664d64164a242783c013d243133c8
tristan.wittgen <tristanwittgen@gmail.com>
2024-12-04 12:22:04 +0100

    #2024 day4 cleanup

--- src/test/kotlin/de/twittgen/aoc/y2024/Day04.kt
+++ src/test/kotlin/de/twittgen/aoc/y2024/Day04.kt
@@ -8,16 +8,16 @@ class Day04 : Day<Puzzle>() {
     override fun String.parse() = mapLines(String::toCharList)
 
     init {
-        part1(18, 2370) {it.mapCoordinates { x, y, _ ->  it.findXmasAt(x,y) }.sum() }
+        part1(18, 2370) { it.mapCoordinates { x, y, _ ->  it.findXmasAt(x,y) }.sum() }
         part2(9, 1908) { it.mapCoordinates { x, y, _ -> it.findCrossMasAt(x,y) }.sum() }
     }
 
+    private val xmas =  listOf('X', 'M', 'A', 'S')
     private fun Puzzle.findXmasAt(x: Int, y: Int) : Int {
-        if (this[x][y] != 'X') return 0
-        val hasMASInDirection = { (dx,dy) : Point2D ->
-            (1..3).map { getOrNullAt(x + (dx * it), y + (dy * it)) } == listOf('M', 'A', 'S')
-        }
-        return directions.map { hasMASInDirection(it) }.count { it }
+        val hasXMASInDirection = { (dx,dy) : Point2D -> (0..3).asSequence().mapIndexed { i, it ->
+            getOrNullAt(x + (dx * it), y + (dy * it)) == xmas[i]
+        }.all { it } }
+        return directions.map { hasXMASInDirection(it) }.count { it }
     }
 
     private fun Puzzle.findCrossMasAt(x:Int, y:Int): Int {
