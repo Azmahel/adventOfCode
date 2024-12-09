5b87287bcbbaf503e54b205e9118949511f825bb
tristan.wittgen <tristanwittgen@gmail.com>
2024-12-09 22:41:20 +0100

    #2023 day 18 remove old part 1

--- src/test/kotlin/de/twittgen/aoc/y2023/Day18.kt
+++ src/test/kotlin/de/twittgen/aoc/y2023/Day18.kt
@@ -8,8 +8,8 @@ import de.twittgen.aoc.util.mapLines
 
 class Day18: Day<List<Instruction>>() {
     override fun String.parse() =
-        mapLines { it.split(" ").let { (a,b,c) -> Instruction(mapDirection(a), b.toLong(), c) } }
-    private fun mapDirection(a: String) = entries.first { it.s == a }
+        mapLines { it.split(" ").let { (a,b,c) -> Instruction(a.toDirection(), b.toLong(), c) } }
+    private fun String.toDirection() = entries.first { it.s == this }
 
     init {
         part1(62, 42317) { picksTheorem(it) }
@@ -21,7 +21,7 @@ class Day18: Day<List<Instruction>>() {
 
     private val colorRegex = Regex("\\(#(.{5})(.)\\)")
     private fun Instruction.fix() = colorRegex.matchEntire(color)!!.groupValues.let { (_,a,b) ->
-        Instruction(mapDirection(when(b) { "0" -> "R" "1" -> "D" "2" ->"L" else -> "U" }), a.toLong(16), "")
+        Instruction(when (b) {"0" -> "R""1" -> "D""2" -> "L"else -> "U" }.toDirection(), a.toLong(16), "")
     }
 
     private fun List<Instruction>.toVertices() = runningFold(0L to 0L) { acc, (dir, dist) ->
@@ -30,37 +30,6 @@ class Day18: Day<List<Instruction>>() {
 
     private fun List<Pair<Long, Long>>.shoelace() = zipWithNext().sumOf { (a, b) -> (a.first * b.second) - (a.second * b.first) } / 2
     private fun List<Instruction>.perimeter() = sumOf { it.dist }
-
-    private fun List<Instruction>.dig(): Set<Point2D> {
-        var current = ORIGIN
-        val map = mutableSetOf<Point2D>()
-        forEach { instruction ->
-            (1..instruction.dist).forEach { _ ->
-                map.add(current)
-                current = instruction.dir.next(current)
-            }
-        }
-        return map
-    }
-
-    private fun Set<Point2D>.fill(): Set<Point2D> {
-        val xR = minOf { it.x } .. maxOf { it.x }
-        val yR = minOf { it.y } .. maxOf { it.y }
-        fun Point2D.isInPit() =
-            (xR.first..x).count { Point2D(it, y) in this@fill } % 2 == 1 && (yR.first..y).count { Point2D(x, it) in this@fill } % 2 == 1
-        fun Point2D.fillMore(): Set<Point2D> {
-            val current = mutableSetOf(this)
-            var next: List<Point2D>
-            do {
-                next = current.flatMap { it.adjacent() }.filter { it !in current && it !in this@fill }
-                current.addAll(next)
-            } while (next.isNotEmpty())
-            return current
-        }
-        return xR.flatMap { x -> yR.mapNotNull { y -> Point2D(x, y) } }
-            .first { it !in this && it.isInPit()  }
-            .fillMore() + this
-    }
 
     override val example = """
         R 6 (#70c710)
