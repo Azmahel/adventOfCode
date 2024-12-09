6ca9229e817e04103c0582794dbab53dd3ff1734
tristan.wittgen <tristanwittgen@gmail.com>
2024-12-09 12:36:29 +0100

    #2024 cleanup

--- src/test/kotlin/de/twittgen/aoc/y2024/Day01.kt
+++ src/test/kotlin/de/twittgen/aoc/y2024/Day01.kt
@@ -11,15 +11,14 @@ class Day01 : Day<Pair<List<Int>,List<Int>>>() {
         mapLines { it.split("   ").let { it.first().toInt() to it.second().toInt()} }.toPairOfLists()
 
     init {
-        part1(11, 2000468) { (a,b) ->
-            (a.sorted() to b.sorted()).toListOfPairs().sumOf { (a, b) -> abs(a - b) }
+        part1(11, 2000468) { (a, b) ->
+            (a.sorted() zip b.sorted()).sumOf { (a, b) -> abs(a - b) }
         }
         part2(31,18567089) { (a,b) ->
             b.entryCount().let {counts  ->  a.sumOf { it * counts.getOrDefault(it, 0) } }
         }
     }
 
-    private  fun <A, B> Pair<List<A>,List<B>>.toListOfPairs() :  List<Pair<A,B>> = first zip second
     private fun List<Int>.entryCount() = groupBy { it }.mapValues { (_, l) -> l.size }
 
     override val example = """
