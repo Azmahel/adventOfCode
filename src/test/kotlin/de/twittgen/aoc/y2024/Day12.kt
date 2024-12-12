3153eb3e5552c4728c20262944b723224ef95611
tristan.wittgen <tristanwittgen@gmail.com>
2024-12-12 14:19:46 +0100

    #cleanup

--- src/test/kotlin/de/twittgen/aoc/y2024/Day12.kt
+++ src/test/kotlin/de/twittgen/aoc/y2024/Day12.kt
@@ -8,9 +8,7 @@ class Day12 : Day<List<Plot>>() {
     override fun String.parse() = toGrid { p, c -> c to p }.toSet().toPlots().flatMap { it.value }
 
     init {
-        part1(1930, 1461806) {
-            it.sumOf { it.fencingCost() }
-        }
+        part1(1930, 1461806) { it.sumOf { it.fencingCost() } }
     }
 
     private fun Plot.fencingCost() = size * sumOf { it.orthogonallyAdjacent().filter { !contains(it) }.size }
@@ -21,21 +19,13 @@ class Day12 : Day<List<Plot>>() {
     private fun Set<Point2D>.toPlots(): Set<Plot> {
         val visited = mutableSetOf<Point2D>()
         val comp = mutableSetOf<Plot>()
-        forEach { p ->
-            if (p !in visited) {
-                val c = dfs(setOf(p))
-                comp += c
-                visited += c
-            }
-        }
+        forEach { p -> if (p !in visited) dfs(setOf(p)).also { comp += it; visited += it } }
         return comp
     }
 
-    private fun Set<Point2D>.dfs(current: Plot): Plot =
-        current.flatMap { it.orthogonallyAdjacent() }.filter { it in this && it !in current }.let {
-            if (it.isEmpty()) current else dfs(current + it)
-        }
-
+    private fun Set<Point2D>.dfs(current: Plot): Plot = current.flatMap { it.orthogonallyAdjacent() }
+        .filter { it in this && it !in current }
+        .let { if (it.isEmpty()) current else dfs(current + it) }
 
     override val example = """
         RRRRIICCFF
